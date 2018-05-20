9a38c53ab6f02dac245f369030935863a398bd4d
basaragakadi <basar..1994>
2018-05-20 14:30:12 +0300

    doAction function has been implemented. Action related function templates are implemented with displaymessages. TODOs are added.

--- termproject.hs
+++ termproject.hs
@@ -11,6 +11,10 @@ type Word = String
 data Action = Add | Search | Find | Print | Exit | Err 
 			deriving (Eq, Show)
 
+main = do
+	printActions
+	act <- getLine
+	doAction $ convertAction act
 
 empty :: Trie
 empty = Trie {end = False , children = M.empty}
@@ -47,12 +51,58 @@ convertAction str
 	| str == "e" = Exit
 	| otherwise  = Err
 
-doAction :: Action
-doAction = undefined
+doAction :: Action -> IO ()
+doAction action
+	| action == Err = printErr
+	| action == Add = addFunction
+	| action == Search = searchFunction
+	| action == Find = findFunction
+	| action == Print = printFunction
+	| action == Exit = exit
 
 printErr :: IO ()
-printErr = putStrLn "Not a valid action."
+printErr = do putStrLn "Not a valid action."
 
 -- ** Necessary functions for Action handling. **
+
+
+-- ** Action related function implementations. **
+
+addFunction :: IO ()
+addFunction = do
+	putStrLn "Enter word/prefix:"
+	line <- getLine
+	putStrLn ("New word is added! -> " ++ line)
+
+searchFunction :: IO ()
+searchFunction = do
+	putStrLn "Enter word/prefix:"
+	line <- getLine
+	putStrLn "Search function result will be displayed here." -- TODO: Another function related to search functionality must be implemented with guards according to the result bool of search function on top.
+
+findFunction :: IO ()
+findFunction = do
+	putStrLn "Enter word/prefix:"
+	line <- getLine
+	putStrLn "Find function result will be displayed here." -- TODO: Another function related to find functionality must be implemented with guards according to the result of prefix function on top.
+
+printFunction :: IO ()
+printFunction = do
+	putStrLn "List of words in dictionary:"
+	-- TODO: Write another function that lists and prints all words in dictionary here.
+	putStrLn "Results\nof\nPrint\nFunction\nwill\nbe\ndisplayed\nhere."
+
+exit :: IO ()
+exit = do
+	putStrLn "Program terminated."
+	-- TODO: Terminate program here.
+
+-- ** Action related function implementations. **
+
+
+
+
+
+
 
 
